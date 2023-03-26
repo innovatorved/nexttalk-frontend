810fa2b0baa87be64cc2789fa35ce8075b106734
Ved Gupta <vedgupta@protonmail.com>
2023-03-27 04:07:19 +0530

    Fixed #10 icons and manifest.json added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,53 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>NextTalk - The Next Generation Chat App</title>
+        <link rel="icon" href="/favicon-dark.ico" />
+        <link
+          rel="icon"
+          href="/favicon.ico"
+          media="(prefers-color-scheme: dark)"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon-dark.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+          media="(prefers-color-scheme: dark)"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32-dark.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+          media="(prefers-color-scheme: dark)"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16-dark.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+          media="(prefers-color-scheme: dark)"
+        />
+        <link rel="manifest" href="/manifest.json"></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
